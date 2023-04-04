40a46b18d55353551fc4fd523814b57c87020e77
lmclmc <lmc8979@qq.com>
2023-04-04 10:21:46 +0800

    更新build.sh

--- build.sh
+++ build.sh
@@ -4,13 +4,14 @@ CURRENT_DIR=$(pwd)
 BASE_BUILD_DIR=${CURRENT_DIR}/base_build/
 BUILD_DIR=${CURRENT_DIR}/build/
 BASE_DIR=${CURRENT_DIR}/base/
+CPU_NUM=$(cat /proc/cpuinfo | grep "physical id" | wc -l)
 
 rm -rf ${BASE_BUILD_DIR}
 mkdir ${BASE_BUILD_DIR}
 pushd ${BASE_BUILD_DIR}
 
 cmake ${BASE_DIR} -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-std=c++11"
-make
+make -j${CPU_NUM}
 make install
 popd
 
@@ -20,5 +21,5 @@ pushd ${BUILD_DIR}
 
 cmake ${CURRENT_DIR} -DCMAKE_INCLUDE_PATH="${BASE_BUILD_DIR}install/include/"
 #-DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-std=c++11"
-make
+make -j${CPU_NUM}
 popd
